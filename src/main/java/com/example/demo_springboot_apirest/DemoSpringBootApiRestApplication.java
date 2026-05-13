59555618b7f8e1861059d07fb323b20fd1e38698
LuciT893 <lucitorres9909@gmail.com>
2026-05-13 14:15:59 -0300

    Initial commit

---
+++ src/main/java/com/example/demo_springboot_apirest/DemoSpringBootApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_springboot_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_springboot_apirest/DemoSpringBootApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_springboot_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
